176060aafec90331abe3334b5eb35994540b1927
Xarybdis <38101330+Xarybdis@users.noreply.github.com>
2019-02-09 00:34:54 +0300

    Before Creating Adapter

---
+++ app/src/main/java/com/example/emrullah/recyclerviewuse/ExampleItem.java
@@ -0,0 +1,9 @@
+package com.example.emrullah.recyclerviewuse;
+
+//we use this class to fill the recyclerview
+public class example_item {
+    private int imageResource;
+    private String mText1;
+    private String mText2;
+    
+}
