74d1831d61e6ff192378bcb400040dfd25cb4020
Xarybdis <38101330+Xarybdis@users.noreply.github.com>
2019-02-09 02:46:43 +0300

    Insert and remove buttons added

--- app/src/main/java/com/example/emrullah/recyclerviewuse/ExampleAdapter.java
+++ app/src/main/java/com/example/emrullah/recyclerviewuse/ExampleAdapter.java
@@ -1,6 +1,61 @@
 package com.example.emrullah.recyclerviewuse;
 
+import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ExampleAdapter extends RecyclerView.Adapter {
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+
+    private ArrayList<ExampleItem> mExampleList;
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder{
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+        public ExampleViewHolder(@NonNull View itemView) {
+            //For our View, we define the items in our ViewHolder Constructor.
+            super(itemView);
+            mImageView=itemView.findViewById(R.id.imageView);
+            mTextView1=itemView.findViewById(R.id.textView1);
+            mTextView2=itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public ExampleAdapter(ArrayList<ExampleItem> exampleItems){
+        //to use the data we need to get items in constructor of Adapter
+        mExampleList=exampleItems;
+    }
+
+    @NonNull
+    @Override//we are inflating the layout of which we are going to show in our recyclerview
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.example_item,viewGroup,false);
+        ExampleViewHolder eVholder = new ExampleViewHolder(view);
+        return eVholder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder exampleViewHolder, int position) {
+        //gets the items from ExampleItem class via ArrayList.
+        //And sets the items to the their places
+        ExampleItem currentItem=mExampleList.get(position);
+
+        exampleViewHolder.mImageView.setImageResource(currentItem.getImageResource());
+        exampleViewHolder.mTextView1.setText(currentItem.getmText1());
+        exampleViewHolder.mTextView2.setText(currentItem.getmText2());
+
+    }
+
+    @Override
+    public int getItemCount() {//it counts items in the list we defined.
+        return mExampleList.size();
+    }
 }
--- app/src/main/java/com/example/emrullah/recyclerviewuse/MainActivity.java
+++ app/src/main/java/com/example/emrullah/recyclerviewuse/MainActivity.java
@@ -2,13 +2,84 @@ package com.example.emrullah.recyclerviewuse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    ArrayList<ExampleItem> mExampleList;
+
+    private EditText editTextInsert;
+    private EditText editTextRemove;
+    private Button buttonInsert;
+    private Button buttonRemove;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello man");
+
+        creatItems();
+        buildRecyclerView();
+
+        editTextInsert=findViewById(R.id.edit_insert);
+        editTextRemove=findViewById(R.id.edit_remove);
+        buttonInsert=findViewById(R.id.button_insert);
+        buttonRemove=findViewById(R.id.button_remove);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position= Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
+    }
+
+
+
+
+    public void insertItem(int position){
+        mExampleList.add(position,new ExampleItem(R.drawable.beer_nav_icon,"New item added","Line"));
+        mAdapter.notifyItemInserted(position);
+    }
+    public void removeItem(int position){
+        mExampleList.remove(position);//Here, we remove item from arrayList
+        mAdapter.notifyItemRemoved(position);//Here, we make adapter to notify that we removed an item.
+    }
+
+    public void buildRecyclerView(){
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter= new ExampleAdapter(mExampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+    public void creatItems(){
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.beer_nav_icon,"Line 1", "Line 2"));
+        mExampleList.add(new ExampleItem(R.drawable.beer_nav_icon,"Line 3", "Line 4"));
+        mExampleList.add(new ExampleItem(R.drawable.beer_nav_icon,"Line 5", "Line 6"));
+        mExampleList.add(new ExampleItem(R.drawable.beer_nav_icon,"Line 7", "Line 8"));
     }
 }
